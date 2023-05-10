3caa9e26c0be669b08ae4d858b79eabf02ed5d22
Ferhan-J-Patel <126552299+Ferhan-J-Patel@users.noreply.github.com>
2023-05-10 19:11:09 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
 # Output the AppStream URL for the test user
 output "appstream_stack_url" {
-  value = "https://${data.aws_appstream_stack_fleet_association.test.fleet_name}.${data.aws_appstream_stack_fleet_association.test.region}.aws.amazon.com/enduser/launchAppstream.html?stack=${aws_appstream_stack.test.name}&username=${var.username}"
+  value = "https://${data.aws_appstream_fleet_stack_association.test.fleet_name}.${data.aws_appstream_fleet_stack_association.test.region}.aws.amazon.com/enduser/launchAppstream.html?stack=${aws_appstream_fleet_stack_association.test.name}&username=${var.username}"
 }
