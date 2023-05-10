0fc6edcf5169fa3d414944a4d9f6734a01592ffc
Ferhan-J-Patel <126552299+Ferhan-J-Patel@users.noreply.github.com>
2023-05-11 02:18:16 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,11 @@
 output "stack_arn" {
   value = "http://cl-auth-site.s3-website-ap-southeast-1.amazonaws.com/window-stack/signin.html"
 }
+
+output "password" {
+  value = var.password
+}
+
+output "Note" {
+  value = "Copy and paste the above url and enter your cloudlab-username and the password given here"
+}
