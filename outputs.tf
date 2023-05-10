1042c177d2dbd864e5e9c843a6d3ac019ef6f213
Ferhan-J-Patel <126552299+Ferhan-J-Patel@users.noreply.github.com>
2023-05-10 14:43:07 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
 # Output the AppStream URL for the test user
 output "appstream_url" {
-  value = data.aws_appstream_fleet.appstream_fleet.url
+  value = resource.aws_appstream_fleet.appstream_fleet.url
 }
