96642c989eb7794d923b8a1aca17c745442171e6
Ferhan-J-Patel <126552299+Ferhan-J-Patel@users.noreply.github.com>
2023-05-10 13:28:29 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,10 @@
+# Define variables for username and password
+variable "username" {
+  type    = string
+  default = "username"
+}
+
+variable "password" {
+  type    = string
+  default = "123456"
+}
