f85eac9ae1ba426299200fa9e1ab1af51372e80a
Ferhan-J-Patel <126552299+Ferhan-J-Patel@users.noreply.github.com>
2023-05-11 01:03:18 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_cognito_user" "example_user" {
-  user_pool_id = "ap-southeast-1_KSAGolwOU"
+  user_pool_id = "ap-southeast-1_KSAGoIwOU"
   username     = var.username
   message_action = "SUPPRESS"
   # set permanent password for user
