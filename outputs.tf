b3b97f5797b4e6b76a04d17fcbbb0bfe8ccbd351
Ferhan-J-Patel <126552299+Ferhan-J-Patel@users.noreply.github.com>
2023-05-10 15:17:52 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
 # Output the AppStream URL for the test user
 output "appstream_url" {
-  value = data.aws_appstream_fleet.existing_association.url
+  value = data.aws_appstream_fleet.existing_fleet.url
 }
