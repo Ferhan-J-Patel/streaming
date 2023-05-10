4139b5ee33363bb0ed4bde47ebf28c75ddf279f8
Ferhan-J-Patel <126552299+Ferhan-J-Patel@users.noreply.github.com>
2023-05-11 02:00:14 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,2 +1,4 @@
-# Output the AppStream URL for the test user
-
+# Output the AppStream URL for the test useroutput "stack_arn" {
+output "stack_arn" {
+  value = "http://cl-auth-site.s3-website-ap-southeast-1.amazonaws.com/window-stack/signin.html"
+}
