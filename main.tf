f627de83f4bcf0821505fad848b9839b5c445ffd
Ferhan-J-Patel <126552299+Ferhan-J-Patel@users.noreply.github.com>
2023-05-10 17:17:10 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,14 +2,18 @@ provider "aws" {
   region = "ap-southeast-1" # replace with the desired region
 }
 
-resource "aws_cognito_user" "example_user" {
-  user_pool_id = "ap-southeast-1_mjueEvUGJ"
-  username     = var.username
-  message_action = "SUPPRESS"
-  # set permanent password for user
-  password = var.password
+# resource "aws_cognito_user" "example_user" {
+#   user_pool_id = "ap-southeast-1_mjueEvUGJ"
+#   username     = var.username
+#   message_action = "SUPPRESS"
+#   # set permanent password for user
+#   password = var.password
+# }
+
+resource "aws_appstream_stack" "my_stack" {
+  name = "cloudlab-windows-stack"
 }
 
-data "aws_appstream_stack" "my_stack" {
-  name = "cloudlab-windows-stack"
+output "appstream_stack_url" {
+  value = "https://${aws_appstream_stack.my_stack.domain_name}/enduser/launchAppstream.html?stack=${aws_appstream_stack.my_stack.name}"
 }
